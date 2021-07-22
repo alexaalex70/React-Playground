789c7676c8695eea58d4f05c562eb1c846b6a002
Alexandru Alexa <alexandru.alexa@amdaris.com>
2021-07-22 20:42:54 +0300

    React Basics & Working with Components

---
+++ React-Basics/React Basics & Working with Components/README.md
@@ -0,0 +1,9 @@
+# React Basics & Working with Components
+
+## What you should know:
+	- react is declarative -you define the 'goal' and let React figure out how to get there
+	- jsx - non-standard syntax which is enabled in React, because it gets complicated in Javascript
+	- React Shadow DOM ( with React elements) and DOM which is updated only when a state or props change and only there
+	- props - pass data through components ( properties )
+	- display dynamic data
+	- composition vs inheritance
---
+++ React-Basics/React Basics & Working with Components/src/components/Expenses/ExpenseItem.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+import { Card } from '../UI/Card'
+import { ExpenseDate } from './ExpenseDate'
+import './ExpenseItem.css'
+
+interface ExpenseItemInterface {
+    title: string,
+    amount: number,
+    date: Date
+}
+
+export const ExpenseItem: FC<ExpenseItemInterface> = ({title, amount, date}) => {
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={date}/>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>{amount}</div>
+            </div>
+        </Card>
+    )
+}
+
+export default ExpenseItem
